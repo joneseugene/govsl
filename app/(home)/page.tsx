466443dd9a68bf5915aa2a2e1e14db31c286b51b
joneseugeneceo <joneseugeneceo@gmail.com>
2026-04-28 18:52:54 +0000

    Remove unused client codes and use sections in home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,29 +1,34 @@
-import Header from "@/components/layout/header/page"
-import { homeNavLinks } from "@/libs/consts/nav.const"
-import HeroSection from "@/components/section/HomeSection/HeroSection/HeroSection.server"
-import PressReleaseSectionServer from "@/components/section/HomeSection/PressRelease/PressReleaseSection.server"
-import AppointmentSectionServer from "@/components/section/HomeSection/Appointment/AppointSection.server"
+import Header from '@/components/layout/header/page';
+import { homeNavLinks } from '@/libs/consts/nav.const';
+import HeroSection from '@/components/section/HomeSection/HeroSection/HeroSection.server';
+import PressReleaseSectionServer from '@/components/section/HomeSection/PressRelease/PressReleaseSection.server';
+import AppointmentSectionServer from '@/components/section/HomeSection/Appointment/AppointmentSection.server';
+import NewsSectionServer from '@/components/section/HomeSection/News/NewsSection.server';
+import PublicationSectionServer from '@/components/section/HomeSection/Publication/PublicationSection.server.tsx';
+import PopularServicesSectionServer from '@/components/section/HomeSection/PopularService/PopularServiceSection.server';
+import MoreSectionServer from '@/components/section/HomeSection/More/MoreSection.server';
+import MDASectionServer from '@/components/section/HomeSection/Mda/MdaSection.server';
 
 export default function Page() {
-    return (
-        <>
-            {/* CLIENT ONLY COMPONENTS */}
-            <Header links={homeNavLinks} />
+  return (
+    <>
+      {/* CLIENT ONLY COMPONENTS */}
+      <Header links={homeNavLinks} />
 
-            <HeroSection suggestions={[]} />
+      <HeroSection suggestions={[]} />
 
-            {/* SERVER SECTIONS */}
-            <PressReleaseSectionServer />
-            <AppointmentSectionServer />
-            {/* 
-            <GovernmentNewsSection />
-            <PublicationsSection />
-            <PopularServicesSection />
-            <MoreSection />
-            <ServiceSection />
+      {/* SERVER SECTIONS */}
+      <PressReleaseSectionServer />
+      <AppointmentSectionServer />
+      <NewsSectionServer />
+      <PublicationSectionServer />
+      <PopularServicesSectionServer />
+      <MoreSectionServer />
+      <MDASectionServer />
+      {/* 
             <MdaSection />
 
             <Footer /> */}
-        </>
-    )
+    </>
+  );
 }
